27bd7d7d32a51e43743968b4f76ca1c4fca61844
hellshark <yahir.lope@icloud.com>
2024-12-30 20:46:31 -0600

    Initial Commit

---
+++ reservas/reservasApp.swift
@@ -0,0 +1,17 @@
+//
+//  reservasApp.swift
+//  reservas
+//
+//  Created by Yahir Lope on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct reservasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
