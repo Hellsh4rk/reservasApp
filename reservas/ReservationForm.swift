5a73c463fa362e1b4357d03d4671fcd0c98b799d
hellshark <yahir.lope@icloud.com>
2025-01-02 19:17:24 -0600

    Update a la vista, para poder trackear las modificaciones de los TextField

---
+++ reservas/ReservationForm.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationForm.swift
+//  reservas
+//
+//  Created by Yahir Lope on 02/01/25.
+//
+
+import SwiftUI
+
+struct ReservationForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReservationForm()
+}
